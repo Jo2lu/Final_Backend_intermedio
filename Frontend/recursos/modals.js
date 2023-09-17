b7f30478758dd1f4483888687623b60a703bf2c9
Jo2lu <martinez.lanedro.jose@gmail.com>
2023-09-17 13:11:05 -0600

    Modals

--- Frontend/recursos/modals.js
+++ Frontend/recursos/modals.js
@@ -1,366 +1,369 @@
-// // Modals
-// // Usar For 
+// Modals
 
-// $(function () {
-// 	$("#test_1").click(function () {
-// 		$(".test_1").modal('show');
-// 	});
-// 	$(".test_1").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_1").click(function () {
+		$(".test_1").modal('show');
+	});
+	$(".test_1").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_2").click(function () {
-// 		$(".test_2").modal('show');
-// 	});
-// 	$(".test_2").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_2").click(function () {
+		$(".test_2").modal('show');
+	});
+	$(".test_2").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_3").click(function () {
-// 		$(".test_3").modal('show');
-// 	});
-// 	$(".test_3").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_3").click(function () {
+		$(".test_3").modal('show');
+	});
+	$(".test_3").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_4").click(function () {
-// 		$(".test_4").modal('show');
-// 	});
-// 	$(".test_4").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_4").click(function () {
+		$(".test_4").modal('show');
+	});
+	$(".test_4").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_5").click(function () {
-// 		$(".test_5").modal('show');
-// 	});
-// 	$(".test_5").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_5").click(function () {
+		$(".test_5").modal('show');
+	});
+	$(".test_5").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_6").click(function () {
-// 		$(".test_6").modal('show');
-// 	});
-// 	$(".test_6").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_6").click(function () {
+		$(".test_6").modal('show');
+	});
+	$(".test_6").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_7").click(function () {
-// 		$(".test_7").modal('show');
-// 	});
-// 	$(".test_7").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_7").click(function () {
+		$(".test_7").modal('show');
+	});
+	$(".test_7").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_8").click(function () {
-// 		$(".test_8").modal('show');
-// 	});
-// 	$(".test_8").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_8").click(function () {
+		$(".test_8").modal('show');
+	});
+	$(".test_8").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_9").click(function () {
-// 		$(".test_9").modal('show');
-// 	});
-// 	$(".test_9").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_9").click(function () {
+		$(".test_9").modal('show');
+	});
+	$(".test_9").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_10").click(function () {
-// 		$(".test_10").modal('show');
-// 	});
-// 	$(".test_10").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_10").click(function () {
+		$(".test_10").modal('show');
+	});
+	$(".test_10").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_11").click(function () {
-// 		$(".test_11").modal('show');
-// 	});
-// 	$(".test_11").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_11").click(function () {
+		$(".test_11").modal('show');
+	});
+	$(".test_11").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_12").click(function () {
-// 		$(".test_12").modal('show');
-// 	});
-// 	$(".test_12").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_12").click(function () {
+		$(".test_12").modal('show');
+	});
+	$(".test_12").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_13").click(function () {
-// 		$(".test_13").modal('show');
-// 	});
-// 	$(".test_13").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_13").click(function () {
+		$(".test_13").modal('show');
+	});
+	$(".test_13").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_14").click(function () {
-// 		$(".test_14").modal('show');
-// 	});
-// 	$(".test_14").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_14").click(function () {
+		$(".test_14").modal('show');
+	});
+	$(".test_14").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_15").click(function () {
-// 		$(".test_15").modal('show');
-// 	});
-// 	$(".test_15").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_15").click(function () {
+		$(".test_15").modal('show');
+	});
+	$(".test_15").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_16").click(function () {
-// 		$(".test_16").modal('show');
-// 	});
-// 	$(".test_16").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_16").click(function () {
+		$(".test_16").modal('show');
+	});
+	$(".test_16").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_17").click(function () {
-// 		$(".test_17").modal('show');
-// 	});
-// 	$(".test_17").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_17").click(function () {
+		$(".test_17").modal('show');
+	});
+	$(".test_17").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_18").click(function () {
-// 		$(".test_18").modal('show');
-// 	});
-// 	$(".test_18").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_18").click(function () {
+		$(".test_18").modal('show');
+	});
+	$(".test_18").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_19").click(function () {
-// 		$(".test_19").modal('show');
-// 	});
-// 	$(".test_19").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_19").click(function () {
+		$(".test_19").modal('show');
+	});
+	$(".test_19").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_20").click(function () {
-// 		$(".test_20").modal('show');
-// 	});
-// 	$(".test_20").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_20").click(function () {
+		$(".test_20").modal('show');
+	});
+	$(".test_20").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_21").click(function () {
-// 		$(".test_21").modal('show');
-// 	});
-// 	$(".test_21").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_21").click(function () {
+		$(".test_21").modal('show');
+	});
+	$(".test_21").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_22").click(function () {
-// 		$(".test_22").modal('show');
-// 	});
-// 	$(".test_22").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_22").click(function () {
+		$(".test_22").modal('show');
+	});
+	$(".test_22").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_23").click(function () {
-// 		$(".test_23").modal('show');
-// 	});
-// 	$(".test_23").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_23").click(function () {
+		$(".test_23").modal('show');
+	});
+	$(".test_23").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_24").click(function () {
-// 		$(".test_24").modal('show');
-// 	});
-// 	$(".test_24").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_24").click(function () {
+		$(".test_24").modal('show');
+	});
+	$(".test_24").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_25").click(function () {
-// 		$(".test_25").modal('show');
-// 	});
-// 	$(".test_25").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_25").click(function () {
+		$(".test_25").modal('show');
+	});
+	$(".test_25").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_26").click(function () {
-// 		$(".test_26").modal('show');
-// 	});
-// 	$(".test_26").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_26").click(function () {
+		$(".test_26").modal('show');
+	});
+	$(".test_26").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_27").click(function () {
-// 		$(".test_27").modal('show');
-// 	});
-// 	$(".test_27").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_27").click(function () {
+		$(".test_27").modal('show');
+	});
+	$(".test_27").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_28").click(function () {
-// 		$(".test_28").modal('show');
-// 	});
-// 	$(".test_28").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_28").click(function () {
+		$(".test_28").modal('show');
+	});
+	$(".test_28").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_29").click(function () {
-// 		$(".test_29").modal('show');
-// 	});
-// 	$(".test_29").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_29").click(function () {
+		$(".test_29").modal('show');
+	});
+	$(".test_29").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_30").click(function () {
-// 		$(".test_30").modal('show');
-// 	});
-// 	$(".test_30").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_30").click(function () {
+		$(".test_30").modal('show');
+	});
+	$(".test_30").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_31").click(function () {
-// 		$(".test_31").modal('show');
-// 	});
-// 	$(".test_31").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_31").click(function () {
+		$(".test_31").modal('show');
+	});
+	$(".test_31").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_32").click(function () {
-// 		$(".test_32").modal('show');
-// 	});
-// 	$(".test_32").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_32").click(function () {
+		$(".test_32").modal('show');
+	});
+	$(".test_32").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_33").click(function () {
-// 		$(".test_33").modal('show');
-// 	});
-// 	$(".test_33").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_33").click(function () {
+		$(".test_33").modal('show');
+	});
+	$(".test_33").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_34").click(function () {
-// 		$(".test_34").modal('show');
-// 	});
-// 	$(".test_34").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_34").click(function () {
+		$(".test_34").modal('show');
+	});
+	$(".test_34").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_35").click(function () {
-// 		$(".test_35").modal('show');
-// 	});
-// 	$(".test_35").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_35").click(function () {
+		$(".test_35").modal('show');
+	});
+	$(".test_35").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_36").click(function () {
-// 		$(".test_36").modal('show');
-// 	});
-// 	$(".test_36").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_36").click(function () {
+		$(".test_36").modal('show');
+	});
+	$(".test_36").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_37").click(function () {
-// 		$(".test_37").modal('show');
-// 	});
-// 	$(".test_37").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_37").click(function () {
+		$(".test_37").modal('show');
+	});
+	$(".test_37").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_38").click(function () {
-// 		$(".test_38").modal('show');
-// 	});
-// 	$(".test_38").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_38").click(function () {
+		$(".test_38").modal('show');
+	});
+	$(".test_38").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_39").click(function () {
-// 		$(".test_39").modal('show');
-// 	});
-// 	$(".test_39").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_39").click(function () {
+		$(".test_39").modal('show');
+	});
+	$(".test_39").modal({
+		closable: true
+	});
+});
 
-// $(function () {
-// 	$("#test_40").click(function () {
-// 		$(".test_40").modal('show');
-// 	});
-// 	$(".test_40").modal({
-// 		closable: true
-// 	});
-// });
+$(function () {
+	$("#test_40").click(function () {
+		$(".test_40").modal('show');
+	});
+	$(".test_40").modal({
+		closable: true
+	});
+});
 
+
+/*
+
+Usar For 
 $(function () {
 	$("#buttonDescrip").click(function () {
 		$(".test_40").modal('show');
@@ -369,3 +372,4 @@ $(function () {
 		closable: true
 	});
 });
+*/	
